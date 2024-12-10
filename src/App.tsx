6494de1a1d354ec748c8e744c292fc72688c9048
AngelUrrego <angelurrego2010@gmail.com>
2024-12-10 11:44:18 -0500

    Cambio en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,11 @@
 import "./App.css";
-import { TonConnectButton } from "@tonconnect/ui-react";
-import { useMainContract } from "./hooks/useMainContract";
-import { useTonConnect } from "./hooks/useTonConnect";
-import { fromNano } from "ton-core";
 
 function App() {
-  const {
-    contract_address,
-    counter_value,
-    contract_balance,
-    sendIncrement,
-    sendDeposit,
-    sendWithrawalRequest
-  } = useMainContract();
+  
 
-  const { connected } = useTonConnect()
   return (
     <div>
-      <div>
-        <TonConnectButton />
-      </div>
-      <div>
-        <div className='Card'>
-          <b>Our contract Address</b>
-          <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
-          <b>Our contract Balance</b>
-          {contract_balance&& 
-          (<div className='Hint'>{fromNano(contract_balance)}</div>)}
-        </div>
-
-        <div className='Card'>
-          <b>Counter Value</b>
-          <div>{counter_value ?? "Loading..."}</div>
-        </div>
-      </div>
-
-      {connected && (
-              <a
-                onClick={() => {
-                  sendIncrement();
-                }}
-              >
-                Increment
-              </a>
-      )}
-      <br />
-      {connected && (
-        <a onClick={() =>{
-          sendDeposit()
-        }}>
-          Request deposit of 1 TON
-        </a>
-      )
-  }
-      <br />
-      {connected && (
-        <a onClick={() =>{
-          sendWithrawalRequest()
-        }}>
-          Request 0.7 TON Withrawal
-        </a>
-      )
-  }
+      <h1>Hola Como tas</h1>
     </div>
   );
 }
